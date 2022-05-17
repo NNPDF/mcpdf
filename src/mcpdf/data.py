633231654d4aae9bd3a764a3284d43b6bb13b5c5
Alessandro Candido <candido.ale@gmail.com>
2022-05-18 00:02:59 +0200

    Import fktable conversion from pineappl branch

--- src/mcpdf/data.py
+++ src/mcpdf/data.py
@@ -47,6 +47,14 @@ def covmat():
 
 @functools.cache
 def theory():
+    """Returns the fktable as a dense numpy array that can be directly
+    manipulated with numpy
+    The return shape is:
+        (ndata, nx, nbasis) for DIS
+        (ndata, nx, nx, nbasis) for hadronic
+    where nx is the length of the xgrid
+    and nbasis the number of flavour contributions that contribute
+    """
     theoryid = API.fit(fit=config["fit"]).as_input()["theory"]["theoryid"]
 
     theory = []
@@ -61,21 +69,40 @@ def theory():
         fkdata["elements"] = []
 
         for fk in spec.fkspecs:
+            # Make the dataframe into a dense numpy array
             df = load_fktable(fk).with_cuts(cuts).sigma
-            shape = tuple(
-                [
-                    df.index.get_level_values(n).max() + 1
-                    for n in range(len(df.index[0]))
-                ]
-                + [max(df.columns) + 1]
-            )
 
-            fkarray = np.zeros(shape)
-            for el in df.iloc:
-                for i, v in el.items():
-                    fkarray[(*el.name, i)] = v
+            # Read up the shape of the output table
+            ndata = fk.ndata
+            nx = len(fk.xgrid)
+            nbasis = df.shape[1]
 
-            fkdata["elements"].append(fkarray)
+            if ndata == 0:
+                if fk.hadronic:
+                    return np.zeros((ndata, nbasis, nx, nx))
+                return np.zeros((ndata, nbasis, nx))
+
+            # First get the data index out of the way
+            # this is necessary because cuts/shifts and for performance reasons
+            # otherwise we will be putting things in a numpy array in very awkward orders
+            ns = df.unstack(level=("data",), fill_value=0)
+            x1 = ns.index.get_level_values(0)
+
+            if fk.hadronic:
+                x2 = ns.index.get_level_values(1)
+                fkarray = np.zeros((nx, nx, ns.shape[1]))
+                fkarray[x2, x1, :] = ns.values
+
+                # The output is (ndata, basis, x1, x2)
+                fktable = fkarray.reshape((nx, nx, nbasis, ndata)).T
+            else:
+                fkarray = np.zeros((nx, ns.shape[1]))
+                fkarray[x1, :] = ns.values
+
+                # The output is (ndata, basis, x1)
+                fktable = fkarray.reshape((nx, nbasis, ndata)).T
+
+            fkdata["elements"].append(fktable)
 
         theory.append(fkdata)
 
