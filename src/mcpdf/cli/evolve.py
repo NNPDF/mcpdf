bbde4efabf4263b923b79973220eb71285ba8a89
Alessandro Candido <candido.ale@gmail.com>
2022-08-10 21:27:52 +0200

    Expose theory ID to CLI

--- src/mcpdf/cli/evolve.py
+++ src/mcpdf/cli/evolve.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import pathlib
+import logging
 
 import click
 
 from . import base
 from ..fit import evolve
 
+_logger = logging.getLogger(__name__)
+
 
 @base.command.command("evolve")
+@click.argument("theory", type=click.INT)
 @click.option(
     "-d",
     "--destination",
@@ -15,9 +19,16 @@ from ..fit import evolve
     default=pathlib.Path.cwd().absolute() / "fits",
     help="Alternative destination path (default: $PWD/fits)",
 )
-def subcommand(destination: pathlib.Path):
-    """Evolve fit"""
+def subcommand(theory: int, destination: pathlib.Path):
+    """Evolve fit
 
-    pdf = evolve.evolve(200)
-    evolve.dump(200, pdf, destination)
-    print("Evolved!")
+    Use THEORY (specified as the NNPDF `theory ID`_) to evove given PDF border
+    condition at the given scale.
+
+    .. _theory ID: https://docs.nnpdf.science/theory/theoryindex.html
+
+    """
+
+    pdf = evolve.evolve(theory)
+    evolve.dump(theory, pdf, destination)
+    _logger.info(f"Fit evolved, stored in '{destination}/mcpdf'")
