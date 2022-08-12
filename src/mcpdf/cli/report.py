91bfa2fa72687c47e77e8e64000c3b5d2c802afd
Alessandro Candido <candido.ale@gmail.com>
2022-08-13 01:04:18 +0200

    Add default reports

--- src/mcpdf/cli/report.py
+++ src/mcpdf/cli/report.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 """Create report to compare fits results."""
+import datetime
 import logging
+import os
 import pathlib
+import shutil
 
 import click
 
-from ..fit import report
+from ..fit import install, report
 from . import base
 
 _logger = logging.getLogger(__name__)
 
 
 @base.command.command("report")
-@click.argument("runcard", type=click.Path(exists=True, path_type=pathlib.Path))
+@click.argument("fit", type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option("--which", type=click.Choice(report.REPORTS), default=report.REPORTS[0])
+@click.option("--runcard", type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option("--reinstall", is_flag=True)
 @click.option(
     "-d",
     "--destination",
@@ -20,12 +26,38 @@ _logger = logging.getLogger(__name__)
     default=pathlib.Path.cwd().absolute() / "reports",
     help="Alternative destination path (default: $PWD/reports)",
 )
-def subcommand(runcard: pathlib.Path, destination: pathlib.Path):
-    """Generate fit reports.
+def subcommand(
+    fit: pathlib.Path,
+    which: str,
+    runcard: pathlib.Path,
+    reinstall: bool,
+    destination: pathlib.Path,
+):
+    """Generate FIT comparison report.
 
-    Produce reports following the template specified by RUNCARD.
+    Produce reports following the template specified by runcard.
 
     """
-    repdir = report.report(runcard)
+    try:
+        install.install(fit, force=reinstall)
+    except FileExistsError:
+        _logger.error(
+            "The PDF set is already present, either rename or force reinstallation."
+        )
+        return
+
+    tmpdir = report.ensure_runcard(runcard, which=which)
+    os.environ["BROWSER"] = ""
+    outdir = report.report(tmpdir)
+
+    repdir = destination / f"{fit.name}"
+    try:
+        shutil.move(outdir, repdir)
+    except shutil.Error:
+        repdir = repdir.with_name(
+            repdir.name + f"-{datetime.datetime.now().isoformat()}"
+        )
+        shutil.move(outdir, repdir)
+    shutil.rmtree(tmpdir)
 
     _logger.info(f"Report generated, you can find in '{repdir}'")
--- src/mcpdf/fit/report.py
+++ src/mcpdf/fit/report.py
@@ -6,10 +6,28 @@ import shutil
 import sys
 import tempfile
 from contextlib import contextmanager
+from typing import Optional
 
 import validphys.app as vp
 
-runcards = pathlib.Path(__file__).parent / "runcards"
+REPORTS = ["base", "short", "full"]
+
+
+def rcpath(which: str) -> pathlib.Path:
+    """Path to pre-defined runcards.
+
+    Parameters
+    ----------
+    which: str
+        the
+
+    Returns
+    -------
+    pathlib.Path
+        path to runcard containing folder
+
+    """
+    return pathlib.Path(__file__).parent / f"runcard-{which}"
 
 
 @contextmanager
@@ -48,13 +66,59 @@ def argv(args: list[str]):
         sys.argv = oldargs
 
 
-def report(runcard: os.PathLike) -> pathlib.Path:
+def extract_runcard(which: str, dest: os.PathLike):
+    """Extract runcard to destination.
+
+    Parameters
+    ----------
+    which: str
+        which runcard to extract
+    dest: os.PathLike
+        where to extract
+
+    """
+    for file in rcpath(which).glob("*"):
+        shutil.copy2(file, dest)
+
+
+def ensure_runcard(
+    runcard: Optional[os.PathLike], which: str = REPORTS[0]
+) -> pathlib.Path:
+    """Generate wrapping folder with runcard.
+
+    Parameters
+    ----------
+    runcard: None or os.PathLike
+    which : str
+
+    Returns
+    -------
+    pathlib.Path
+        path to temporary folder, containing requested runcard
+
+    """
+    tmpdir = pathlib.Path(tempfile.mkdtemp())
+
+    if runcard is None:
+        extract_runcard(which, tmpdir)
+    else:
+        runcard = pathlib.Path(runcard)
+        if runcard.is_file():
+            shutil.copy2(runcard, tmpdir / "runcard.yaml")
+        elif runcard.is_dir():
+            for file in runcard.glob("*"):
+                shutil.copy2(file, tmpdir)
+
+    return tmpdir
+
+
+def report(workdir: os.PathLike):
     """Produce comparison report.
 
     Parameters
     ----------
-    runcard: os.PathLike
-        path to runcard to use for comparison
+    workdir: os.PathLike
+        path to folder containing report
 
     Returns
     -------
@@ -62,13 +126,10 @@ def report(runcard: os.PathLike) -> pathlib.Path:
         path to generated report folder
 
     """
-    runcard = pathlib.Path(runcard)
+    workdir = pathlib.Path(workdir)
 
-    tmpdir = pathlib.Path(tempfile.mkdtemp())
-    shutil.copy2(runcard, tmpdir / runcard.name)
-
-    with cwd(tmpdir):
-        with argv(f"vp {runcard.name}".split()):
+    with cwd(workdir):
+        with argv("validphys runcard.yaml".split()):
             vp.main()
 
-    return tmpdir / "output"
+    return workdir / "output"
